2a2210c9617016dea6b98765a76634465778835b
ahmedheidar <ahmedsabokhater@gmail.com>
2023-08-03 14:45:45 +0200

    Stripe payment methods

--- client/stripecheckout/src/app/payments/checkout/page.jsx
+++ client/stripecheckout/src/app/payments/checkout/page.jsx
@@ -1,28 +1,111 @@
 import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
 
-
-
+const stripe = await loadStripe(
+  "pk_test_51NGnGsHWWssUp0efQhuaRItb7FlRhAQIUbLfKC8t9Jet4QpnfnLGSRKBEiOz3A0rBfclw5Q0AtAAXaUgfV8nyNIo00CCNSkGGn"
+);
+let clientSecret;
 const response = async (plan) => {
-    const res =  await fetch('http://localhost:5000/api/payment/create-checkout-session', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ plan: plan })
-    });
-    
-    const session = await res.json();
-    return session;
-}
-
-
-export async function payment(plan) {
-    const session = await response(plan);
-    const stripe = await loadStripe("pk_test_51NGnGsHWWssUp0efQhuaRItb7FlRhAQIUbLfKC8t9Jet4QpnfnLGSRKBEiOz3A0rBfclw5Q0AtAAXaUgfV8nyNIo00CCNSkGGn");
-    const { error } = await stripe.redirectToCheckout({
-        sessionId: session.id,
-    });
-    if (error) {
-        console.log(error);
+  const res = await fetch(
+    "http://localhost:5000/connect/create-checkout-session",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ plan: plan }),
     }
+  );
+
+  const session = await res.json();
+  return session;
+};
+
+ async function payment(plan) {
+  const session = await response(plan);
+  const { error } = await stripe.redirectToCheckout({
+    sessionId: session.id,
+  });
+  if (error) {
+    console.log(error);
+  }
 }
+
+async function payment2(amount) {
+  const res = await fetch(
+    "http://localhost:5000/payments/create-payment-intent",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: amount }),
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch payment intent");
+  }
+
+  const paymentIntent = await res.json();
+  return paymentIntent; // Return the paymentIntent object
+}
+
+const CheckOutPage = () => {
+  const elements = stripe.elements();
+  const cardElement = elements.create("card");
+  cardElement.mount("#card-element");
+  const [clientSecret, setClientSecret] = useState(null);
+
+  useEffect(() => {
+    // Call payment2 function to get the clientSecret
+    const fetchClientSecret = async () => {
+      try {
+        const result = await payment2(15000);
+        setClientSecret(result.clientSecret);
+      } catch (error) {
+        // Handle error here
+      }
+    };
+
+    fetchClientSecret();
+  }, [amount]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+    if (!error) {
+      const { id } = paymentMethod;
+      try {
+        const { error } = await stripe.confirmCardPayment(clientSecret, {
+          payment_method: id,
+        });
+        if (error) {
+          throw new Error(error.message);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    // call payment2
+    <div>
+      <form id="payment-form">
+        <div id="card-element"></div>
+
+        <button id="submit-payment" onClick={handleSubmit}>
+          Pay Now
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CheckOutPage;
--- client/stripecheckout/src/app/plans/page.jsx
+++ client/stripecheckout/src/app/plans/page.jsx
@@ -6,7 +6,8 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 // import CheckoutForm from '@payments/checkout';
 import "./page.styles.css";
-import { payment } from "../payments/checkout/page";
+import { payment, payment2 } from "../payments/checkout/page";
+import CheckOutPage from "../payments/checkout/page";
 
 const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
 
@@ -15,7 +16,7 @@ const PlansPage = () => {
     <div className="plans-page">
       <h1 style={{ color: "black" }}>Plans</h1>
       <div className="plans-container">
-        <div className="plan">
+        {/* <div className="plan">
           <h2>Premium</h2>
           <p>Access to all the features</p>
           <p>Price: $50.00</p>
@@ -26,6 +27,13 @@ const PlansPage = () => {
           <p>Access to some of the features</p>
           <p>Price: $10.00</p>
           <button className="subscribe-button" onClick={()=>{payment('price_1NIK4PHWWssUp0efsIL1UqXL')}}>Subscribe</button>
+        </div> */}
+        <div className="plan">
+          {/* <h2>Early Subscription</h2>
+          <p>Get a special offer for subscribing now!</p>
+          <p>Price: $150.00</p>
+          <button className="subscribe-button" onClick={()=>{CheckOutPage(15000)}}>Subscribe</button> */}
+          <CheckOutPage/>
         </div>
       </div>
     </div>
